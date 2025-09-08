fc59783c0bf6a92c6dbb28d2971e37467ef40823
tibiflorescu1 <tibiflorescu@gmail.com>
2025-09-08 23:10:22 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
