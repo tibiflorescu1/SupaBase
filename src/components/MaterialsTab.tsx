03dd29c4d6e3135a24805eede4ed0d6c4b670ea4
tibiflorescu1 <tibiflorescu@gmail.com>
2025-09-08 21:51:48 +0300

    Updated MaterialsTab.tsx

--- src/components/MaterialsTab.tsx
+++ src/components/MaterialsTab.tsx
@@ -514,7 +514,6 @@ export default function MaterialsTab({
                         }
                       }
                     }}
-                    }}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   />
                 </div>
