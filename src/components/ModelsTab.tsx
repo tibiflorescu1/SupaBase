9286e621578a26450727b4eeb48bd606080449ed
tibiflorescu1 <tibiflorescu@gmail.com>
2025-10-01 12:58:06 +0300

    Updated ModelsTab.tsx

--- src/components/ModelsTab.tsx
+++ src/components/ModelsTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Plus, Eye, Settings2, Edit2, Trash2, Download, Upload, X, Save, ExternalLink, Car } from 'lucide-react';
+import { Plus, Eye, Settings2, CreditCard as Edit2, Trash2, Download, Upload, X, Save, ExternalLink, Car } from 'lucide-react';
 import { AppData, Vehicul, Acoperire, OptiuneExtra } from '../hooks/useSupabaseData';
 
 interface ModelsTabProps {
